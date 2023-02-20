f2ffb0b956cec6161c09d7c4a01b6259e3ee5b6c
Daryna Savytska <dasav@smart-it.com>
2023-02-20 23:03:05 +0200

    Try add demo

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ContainerInfo from './ContainerInfo.tsx';
-import { createRoot } from 'react-dom/client';
-
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-
-root.render(<ContainerInfo />);
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import ContainerInfo from './ContainerInfo';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+
+root.render(<ContainerInfo />);
